dbafaa65da42b75cc581c633263433642ad99fdf
mahalakshmiganugula12 <102350492+mahalakshmiganugula12@users.noreply.github.com>
2022-08-30 20:39:19 +0530

    n=int(input()) l=list(map(int,input().split())) d=[] s=0 for i in l:     if l.count(i)==i:         s=1         if i in d:             continue         d.append(i) if s==0:     print("-1") else:     print(*d)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,21 +1,14 @@
 n=int(input())
 l=list(map(int,input().split()))
 d=[]
-c=[]
 s=0
 for i in l:
     if l.count(i)==i:
         s=1
         if i in d:
-            
             continue
-        
-        
         d.append(i)
 if s==0:
     print("-1")
 else:
     print(*d)
-        
- 
-        
