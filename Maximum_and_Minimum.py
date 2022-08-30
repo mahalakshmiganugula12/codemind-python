44e64b78f449f7ad7c753eecadb773b3af749900
mahalakshmiganugula12 <102350492+mahalakshmiganugula12@users.noreply.github.com>
2022-08-30 20:16:25 +0530

    n=int(input()) l=list(map(int,input().split())) d=[] s=0 for i in l:     if l.count(i)==i:         s=1         if i in d:             continue         d.append(i) if s==0:     print("-1") else:     print(min(d),max(d))

---
+++ Maximum_and_Minimum.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+d=[]
+s=0
+for i in l:
+    if l.count(i)==i:
+        s=1
+        if i in d:
+            continue
+        d.append(i)
+if s==0:
+    print("-1")
+else:
+    print(min(d),max(d))
